065517ea16cd8f1567a69704fb0bbdb76bce9357
Ryan Heupler <Rheupler@gmail.com>
2015-08-28 09:01:58 -0700

    adds validation for new venue names

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,7 +2,9 @@ require('spec_helper')
 
 describe(Band) do
 
-  it("validates a new name") do
+  it { should have_and_belong_to_many(:venues) }
+
+  it("validates the presence of name") do
     new_band = Band.new({:name => ""})
     expect(new_band.save()).to(eq(false))
   end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,1 +1,12 @@
 require('spec_helper')
+
+describe(Venue) do
+
+    it { should have_and_belong_to_many(:bands) }
+
+    it('validates new venue name') do
+      new_venue = Venue.new({name: ""})
+      expect(new_venue.save()). to eq(false)
+    end
+
+end
