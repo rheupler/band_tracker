1a14ad58892a48e5e63d8113d855588f05fa6efc
Ryan Heupler <Rheupler@gmail.com>
2015-08-28 10:31:12 -0700

    CRUD for bands works

--- app.rb
+++ app.rb
@@ -16,16 +16,26 @@ get '/bands' do
   erb(:bands)
 end
 
-# post('/bands') do
-#   name = params.fetch("band")
-#   Band.create({:name => name})
-#   @bands = Band.all()
-#   erb(:bands)
-# end
-
 post '/bands' do
   name = params.fetch('band_name')
   @band = Band.create({name: name})
+  @bands = Band.all()
+  erb(:bands)
+end
+
+delete '/bands/:id' do
+  id = params.fetch('id')
+  @band = Band.find(id)
+  @band.destroy()
+  @bands = Band.all()
+  erb(:bands)
+end
+
+patch '/bands/:id/update' do
+  id = params.fetch('id')
+  @band = Band.find(id)
+  band_name = params.fetch('band_name')
+  @band.update({name: band_name})
   @bands = Band.all()
   erb(:bands)
 end
