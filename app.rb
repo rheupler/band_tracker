dd5730d4bd1bbb4f588a0bb7cbbd5caeed6caa63
Ryan Heupler <Rheupler@gmail.com>
2015-08-28 10:14:14 -0700

    working add bands page

--- app.rb
+++ app.rb
@@ -3,6 +3,29 @@ require 'pry'
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+##### INDEX #####
+
 get ('/') do
   erb(:index)
+end
+
+##### BANDS #####
+
+get '/bands' do
+  @bands = Band.all()
+  erb(:bands)
+end
+
+# post('/bands') do
+#   name = params.fetch("band")
+#   Band.create({:name => name})
+#   @bands = Band.all()
+#   erb(:bands)
+# end
+
+post '/bands' do
+  name = params.fetch('band_name')
+  @band = Band.create({name: name})
+  @bands = Band.all()
+  erb(:bands)
 end
